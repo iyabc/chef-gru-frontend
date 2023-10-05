af8ff6114d1bc2047b237a797b86aca05d5d2a23
iyabc <juliennepanes@gmail.com>
2023-10-05 17:52:25 +0800

    refactor: removed unused variables, deleted dropdownners component

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,13 +1,10 @@
-import DropdownNERs from '@/components/ui/DropdownNERs';
 import MainButton from '@/components/ui/MainButton';
-import { NERs } from '@/data/nersData';
 
 const HomePage = () => {
   return (
     <div className="h-screen w-screen flex justify-center items-center">
       <div className="flex flex-col justify-center items-center space-y-8">
         <h1 className="text-3xl font-bold">Chef L-BERT</h1>
-        <DropdownNERs ners={NERs} />
         <MainButton text="Generate" />
       </div>
     </div>
