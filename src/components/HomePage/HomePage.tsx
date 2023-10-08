5c053f1a5339dbefd18700bac2471eb1ebc6143b
Innhahn <jaytan3825@gmail.com>
2023-10-08 17:08:41 +0800

    chore: added fonts

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -6,7 +6,9 @@ const HomePage = () => {
   return (
     <div className="h-screen w-screen flex justify-center items-center">
       <div className="flex flex-col justify-center items-center space-y-8">
-        <h1 className="text-3xl font-bold">Chef L-BERT</h1>
+        <h1 className="text-3xl font-rightGroteskCompactBlack font-bold">
+          CHEF L-BERT
+        </h1>
         <DropdownNERs ners={NERs} />
         <MainButton text="Generate" />
       </div>
