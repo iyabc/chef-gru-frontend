e10a38e991142534b66e38dc2374aea01dbcc030
iyabc <juliennepanes@gmail.com>
2023-10-10 13:53:33 +0800

    feat: added hero section

---
+++ src/components/HomePage/HeroSection.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Image from 'next/image';
+
+import BERTLogo from 'public/images/BERT Logo.png';
+
+import MainButton from '@/components/ui/MainButton';
+
+const HeroSection = () => {
+  return (
+    <section className="container py-10 flex flex-col lg:flex-row justify-center items-center lg:grid-cols-2 h-screen text-center lg:text-start">
+      <div className="order-last lg:order-firs w-fit">
+        <div>
+          <h1 className="font-rightGroteskCompactBlack mb-4 mt-7 text-3xl sm:text-4xl md:text-5xl lg:text-7xl">
+            Don&apos;t know what to cook?
+          </h1>
+          <p className="text-sm sm:text-base">
+            Let <span className="font-bold">CHEF L-BERT</span> do the thinking!
+            Just input your ingredients and we&apos;ll try and provide you with
+            a recipe to follow.
+          </p>
+        </div>
+        <div className="mt-5 sm:mt-10 flex justify-center lg:justify-start">
+          <MainButton text="Generate" variant="secondary-outlined" />
+        </div>
+      </div>
+      <div className="relative h-1/2 w-full lg:h-4/5 order-first lg:order-last">
+        <Image
+          src={BERTLogo}
+          alt="BERTLogo"
+          className="object-contain w-full h-full absolute top-0 left-0"
+          draggable={false}
+          placeholder="blur"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,31 +1,17 @@
 'use client';
 
-import { useRef, useState } from 'react';
-import type { MultiValue } from 'react-select';
+import { useRef } from 'react';
 
-import type { NERType } from 'lib/types/nerType';
-
-import IngredientsDropdownMenu from '@/components/ui/IngredientsDropdownMenu';
+import HeroSection from '@/components/HomePage/HeroSection';
 import MainButton from '@/components/ui/MainButton';
 import NavigationBar from '@/components/ui/NavigationBar';
 
 const HomePage = () => {
   const sectionsRef = useRef(null);
-  const [selectedNERs, setSelectedNERs] = useState<NERType[]>([]);
-
-  const handleValuesChange = (selectedValues: MultiValue<NERType>) => {
-    setSelectedNERs(selectedValues as NERType[]);
-  };
 
   return (
     <div className="w-full">
-      <div className="flex flex-col justify-center items-center space-y-8 h-screen">
-        <h1 className="text-3xl font-bold">CHEF L-BERT</h1>
-        <IngredientsDropdownMenu
-          selectedValues={selectedNERs}
-          onValuesChange={handleValuesChange}
-        />
-      </div>
+      <HeroSection />
       <div>
         <NavigationBar sectionRef={sectionsRef} />
         <div ref={sectionsRef}>
