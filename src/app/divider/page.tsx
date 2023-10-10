3d5f47ad4c8a8a9507640b61879c90aa471fc246
Innhahn <jaytan3825@gmail.com>
2023-10-10 16:31:24 +0800

    feat(Divider): added the component divider with cva for the 2 divider

---
+++ src/app/divider/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Divider from '@/components/ui/Divider';
+
+const page = () => {
+  return (
+    <div>
+      page
+      <Divider
+        title="GENERATE NOVEL RECIPES WITH THE TOUCH OF YOUR HAND"
+        text="Elevate Your Cooking Game with Novel Recipes at Your Fingertips! Harness the Power of Artificial Intelligence to Craft Unique Dishes Tailored to Your Taste Buds. Experience a Revolution in Recipe Generation!"
+        color="Yellow"
+      />
+      <Divider
+        title="Recipe Generation: Unveiling the Process"
+        text="Explore our project's journey from data prep to model training, unveiling the magic of generating unique recipes. Witness how our AI crafts delectable dishes, putting culinary creativity at your fingertips!"
+        color="Black"
+      />
+    </div>
+  );
+};
+
+export default page;
