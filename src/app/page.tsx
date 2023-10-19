bb65916f227cea74db811d62c7c9386348752974
Innhahn <jaytan3825@gmail.com>
2023-10-19 12:28:52 +0800

    feat: added error404Page and Not-found page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+
+import { useEffect, useState } from 'react';
 import HomePage from '@/components/HomePage/HomePage';
+import Custom404 from '@/components/ui/Custom404';
+
 export default function Home() {
-  return (
-    <>
-      <HomePage />
-    </>
-  );
+  const pathName = usePathname();
+  const [isHomePage, setIsHomePage] = useState(true);
+
+  useEffect(() => {
+    if (pathName !== '/') {
+      setIsHomePage(false);
+      console.log('fake pathName' + pathName);
+    } else {
+      setIsHomePage(true);
+      console.log(pathName);
+    }
+  }, [pathName]);
+
+  return <>{isHomePage ? <HomePage /> : <Custom404 />}</>;
 }
