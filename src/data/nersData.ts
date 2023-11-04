5e632698b258e9c15c1a0fe87cbc7904dd49383f
iyabc <juliennepanes@gmail.com>
2023-11-04 22:43:49 +0800

    chore: relocated ner data

--- src/data/nersData.ts
+++ src/data/nersData.ts
@@ -1,4 +1,5 @@
-import finalNersData from 'for ners/final_ners.json';
 import type { NERType } from 'lib/types/nerType';
+
+import finalNersData from '@/data/final_ners.json';
 
 export const ner_options: NERType[] = finalNersData;
