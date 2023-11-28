e9683fdbed02ced2b5c54a14fed68469b491c5f3
Innhahn <jaytan3825@gmail.com>
2023-11-28 15:04:56 +0800

    feat: added downloadable pdf of paper in the footer section

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -7,9 +7,15 @@ import GRULogo from 'public/images/GRU logo.png';
 const Footer = () => {
   return (
     <div className="bg-accent flex bottom-0 justify-center items-center w-full py-[1.56rem]">
-      <button className="relative w-10 h-10">
-        <Image src={GRULogo} alt="GRULogo" className="object-contain" fill />
-      </button>
+      <a
+        href="/pdf/Bungag-Panes-Tan-Chef-BiGRU.pdf"
+        download="Bungag-Panes-Tan-Chef-BiGRU"
+      >
+        <button className="relative w-10 h-10">
+          <Image src={GRULogo} alt="GRULogo" className="object-contain" fill />
+        </button>
+      </a>
+
       <a href="https://github.com/iyabc/chef-l-bert">
         <FaGithubSquare className="w-[2rem] h-[2rem] hover:-translate-y-0.5 text-white transition duration-150 ease-out active:ease-in" />
       </a>
